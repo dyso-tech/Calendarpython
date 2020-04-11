cab1d8617a7735931481ffa20c984bbd186a663a
dyso-tech <54769251+dyso-tech@users.noreply.github.com>
2020-04-11 12:52:17 +0200

    Add files via upload

---
+++ Turtle.py
@@ -0,0 +1,22 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+def square():
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+
+
+square()
+my_turtle.forward(150)
+square()
+
+turtle.done()
+
+
+
